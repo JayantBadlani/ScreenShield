5ac0c9fd4871223411921a2c0f8c4d7a58cb7040
Jayant Badlani <badlanijayant9@gmail.com>
2024-07-03 23:40:37 +0530

    Example update

--- Example/UIKit Example/AppDelegate.swift
+++ Example/UIKit Example/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ScreenShield
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         // Override point for customization after application launch.
+        ScreenShield.shared.protectFromScreenRecording()
         return true
     }
 
